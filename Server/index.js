4bdbda823958a0f2611f2828c156c20057f8a18e
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-12-21 16:46:06 +0400

    changed the dist in index.js and the scripts

--- Server/index.js
+++ Server/index.js
@@ -108,11 +108,16 @@ app.post('/upload-image', upload.single('image'), async (req, res) => {
   });
 }); */
 // Serve static files from the Vite build folder
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// Serve the index.html file for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+// API routes
+app.get("/api", (req, res) => {
+    res.json({ message: "API is working!" });
+});
+
+// Serve React app for all other routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 // Start the server
 app.listen(port, () => {
